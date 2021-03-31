b32678712099dbd786e7cf24bbacc24cbd6bc241
cmosley <cmosley@beholdmedia.com>
2021-03-31 08:53:30 -0500

    chart js styling

--- public/javascripts/sales.js
+++ public/javascripts/sales.js
@@ -6,38 +6,38 @@ let myChart = document.getElementById('myChart').getContext('2d');
 //  chart js testing 
 
 var salesChart = new Chart(myChart, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Net Sales by Day',
+            data: [7500, 5000, 6200, 3800, 4500, 6000, 7000 ],
+            borderColor: "#2c7be5",
+            backgroundColor: "rgba(255,255,255,0.4)",
+            
         }]
     },
     options: {
+        hover: { mode: "label" },
         scales: {
-            yAxes: [{
+            xAxes: [
+              {
+                scaleLabel: {
+                  show: true,
+                  labelString: "Month",
+                },
                 ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+                  fontColor: "#fff",
+                  fontStyle: 600,
+                },
+                gridLines: {
+                  color: "#fff",
+                  zeroLineColor: "#fff",
+                  lineWidth: 1,
+                },
+              },
+            ],
+            yAxes: [{ display: false }],
+          },
     }
 });
